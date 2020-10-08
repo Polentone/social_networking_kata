d3d0ad44fb4b4d944ad5ee9ebec75e50b348683f
Matteo <matteo.campestrini@argentea.it>
2020-10-08 22:23:27 +0200

    add second test- when user has two messages then also the second one is retrieved

--- src/test/java/kata/InMemoryPostsTest.java
+++ src/test/java/kata/InMemoryPostsTest.java
@@ -3,6 +3,7 @@ package kata;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.AnnotatedArrayType;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
@@ -10,6 +11,7 @@ import static org.assertj.core.api.Assertions.*;
 class InMemoryPostsTest {
     public static final String USERNAME = "Alice";
     public static final String MESSAGE = "I love the weather today";
+    public static final String ANOTHER_MESSAGE = "even though it's raining";
 
     @Test
     void when_user_post_a_message_then_is_present_when_reading() {
@@ -20,5 +22,19 @@ class InMemoryPostsTest {
 
         assertThat(messages).hasSize(1);
         assertThat(messages.get(0)).isEqualTo(MESSAGE);
+    }
+
+
+    @Test
+    void when_user_post_two_messages_then_they_are_both_retrieved() {
+        Posts posts = new InMemoryPosts();
+        posts.post(USERNAME, MESSAGE);
+        posts.post(USERNAME, ANOTHER_MESSAGE);
+
+        List<String> messages = posts.read(USERNAME);
+
+        assertThat(messages).hasSize(2);
+        assertThat(messages.get(0)).isEqualTo(MESSAGE);
+        assertThat(messages.get(1)).isEqualTo(ANOTHER_MESSAGE);
     }
 }
