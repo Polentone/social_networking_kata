6cffb0b9c7e5a1e6e0fafdd9403e4c9803ae0fb1
Matteo <matteo.campestrini@argentea.it>
2020-10-08 22:27:12 +0200

    add tests for user with no posts and with multiple users

--- src/test/java/kata/InMemoryPostsTest.java
+++ src/test/java/kata/InMemoryPostsTest.java
@@ -3,19 +3,25 @@ package kata;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.lang.reflect.AnnotatedArrayType;
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class InMemoryPostsTest {
     public static final String USERNAME = "Alice";
+    public static final String ANOTHER_USERNAME = "Bobby";
     public static final String MESSAGE = "I love the weather today";
     public static final String ANOTHER_MESSAGE = "even though it's raining";
 
+    private Posts posts;
+
+    @BeforeEach
+    void setUp() {
+        posts = new InMemoryPosts();
+    }
+
     @Test
     void when_user_post_a_message_then_is_present_when_reading() {
-        Posts posts = new InMemoryPosts();
         posts.post(USERNAME, MESSAGE);
 
         List<String> messages = posts.read(USERNAME);
@@ -24,10 +30,8 @@ class InMemoryPostsTest {
         assertThat(messages.get(0)).isEqualTo(MESSAGE);
     }
 
-
     @Test
     void when_user_post_two_messages_then_they_are_both_retrieved() {
-        Posts posts = new InMemoryPosts();
         posts.post(USERNAME, MESSAGE);
         posts.post(USERNAME, ANOTHER_MESSAGE);
 
@@ -36,5 +40,23 @@ class InMemoryPostsTest {
         assertThat(messages).hasSize(2);
         assertThat(messages.get(0)).isEqualTo(MESSAGE);
         assertThat(messages.get(1)).isEqualTo(ANOTHER_MESSAGE);
+    }
+
+    @Test
+    void when_two_users_post_a_message_then_the_correct_one_is_retrieved() {
+        posts.post(USERNAME, MESSAGE);
+        posts.post(ANOTHER_USERNAME, ANOTHER_MESSAGE);
+
+        List<String> messages = posts.read(USERNAME);
+
+        assertThat(messages).hasSize(1);
+        assertThat(messages.get(0)).isEqualTo(MESSAGE);
+    }
+
+    @Test
+    void if_a_user_has_no_messages_then_an_empty_list_is_returned() {
+        List<String> messages = posts.read(USERNAME);
+
+        assertThat(messages).hasSize(0);
     }
 }
